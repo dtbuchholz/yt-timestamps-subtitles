0bfe12e0e203efe94c5c696747e60becd655aec2
Dan Buchholz <dbuchholz30@gmail.com>
2023-12-15 16:22:05 -0800

    init

---
+++ README.md
@@ -0,0 +1,71 @@
+# YouTube Timestamp & Subtitle Generator
+
+[![License](https://img.shields.io/github/license/tablelandnetwork/docs.svg)](./LICENSE)
+[![standard-readme compliant](https://img.shields.io/badge/standard--readme-OK-green.svg)](https://github.com/RichardLitt/standard-readme)
+[![](https://img.shields.io/badge/made%20by-Textile%20-blue.svg?style=flat-square)](http://textile.io)
+
+> YouTube video timestamp and subtitle generator using OpenAI's whisper and ChatGPT-4 models
+
+## Table of Contents
+
+- [Background](#background)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Background
+
+This provides a simple script to generate YouTube timestamps and subtitles from a video file using OpenAI's whisper and ChatGPT-4 models. It outputs a `segments.srt` file and a `timestamps.txt` file, which can be used when uploading a new video to YouTube.
+
+## Usage
+
+First, make sure you set up an OpenAI API key/organization and set it as an environment variable in your `.env` file:
+
+```shell
+OPENAI_API_ORG=your_org_id
+OPENAI_API_KEY=your_api_key
+```
+
+Then, clone this repo and install dependencies:
+
+```shell
+git clone https://github.com/dtbuchholz/yt-timestamps-subtitles.git
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+Lastly, run the script, passing a path to your video file:
+
+```shell
+python3 main.py --file /path/to/video.mp4
+```
+
+The output will generate a `segments.srt` file for subtitles with something like the following:
+
+```txt
+1
+00:00:00,000 --> 00:00:09,599
+The Tableland Studio is designed to let you interact with the Tableland network from the comfort of a web application or CLI to create teams, projects and tables.
+
+2
+00:00:10,599 --> 00:00:14,480
+There are a number of features that it offers and we'll walk through them here today.
+```
+
+And a corresponding YouTube timestamps will take this into consideration when creating the `timestamps.txt` file to be used in the YouTube video's description:
+
+```txt
+0:00 - Introduction to Tableland Studio
+0:15 - Logging into Tableland web app
+```
+
+## Contributing
+
+PRs accepted. Please review additional details explained in the [contributing](https://docs.tableland.xyz/contribute) section of the docs site.
+
+Small note: If editing the README, please conform to the [standard-readme](https://github.com/RichardLitt/standard-readme) specification.
+
+## License
+
+MIT AND Apache-2.0, © 2021-2022 Tableland Network Contributors
---
+++ main.py
@@ -0,0 +1,263 @@
+"""Transcribe a video file and generate YouTube timestamps."""
+from argparse import ArgumentParser
+from os import getenv
+
+import whisper
+from dotenv import find_dotenv, load_dotenv
+from moviepy.editor import VideoFileClip
+from openai import OpenAI
+
+# Load env vars for OpenAI API and organization
+load_dotenv(find_dotenv())
+openai_api_key = getenv("OPENAI_API_KEY")
+openai_api_org = getenv("OPENAI_API_ORG")
+
+# Load OpenAI API client
+client = OpenAI(
+    organization=openai_api_org,
+    api_key=openai_api_key,
+)
+
+# Load whisper model
+options = whisper.DecodingOptions(fp16=False, language="en")
+whisper_model = whisper.load_model("base.en")
+
+
+def cmd() -> str:
+    """
+    Parse command line arguments for video file path.
+
+    Returns
+    -------
+        str
+            Path to video file.
+    """
+
+    parser = ArgumentParser(
+        description="Transcribe a video file and generate YouTube timestamps."
+    )
+    parser.add_argument(
+        "--file",
+        type=str,
+        default=None,
+        help="Input video file path to transcribe.",
+    )
+    args = parser.parse_args()
+    return args.file
+
+
+def get_video_duration(video_path: str) -> str:
+    """
+    Get the duration of a video file.
+
+    Parameters
+    ----------
+        video_path : str
+            Path to video file.
+
+    Returns
+    -------
+        str
+            Duration of video file in HH:MM:SS format.
+    """
+    with VideoFileClip(video_path) as video:
+        duration = video.duration
+        hours = int(duration // 3600)
+        minutes = int((duration % 3600) // 60)
+        seconds = int(duration % 60)
+        return f"{hours}:{minutes}:{seconds}"
+
+
+def transcribe(video_path) -> str:
+    """
+    Transcribe a video file with the whisper model.
+
+    Parameters
+    ----------
+        video_path : str
+            Path to video file.
+
+    Returns
+    -------
+        str
+            Transcription of video file.
+    """
+    result = whisper_model.transcribe(video_path, **options.__dict__, verbose=False)
+    return result
+
+
+def segments_to_srt(segments) -> str:
+    """
+    Convert segments to SRT format.
+
+    Parameters
+    ----------
+        segments : list
+            List of segments from whisper model.
+
+    Returns
+    -------
+        str
+            SRT formatted segments.
+    """
+    srt_text = []
+    for i, segment in enumerate(segments):
+        # SRT index starts from 1
+        srt_index = i + 1
+        srt_text.append(str(srt_index))
+
+        # Formatting start and end times
+        start_time = format_time(segment["start"])
+        end_time = format_time(segment["end"])
+        srt_text.append(f"{start_time} --> {end_time}")
+
+        # Adding text
+        srt_text.append(segment["text"].strip())
+
+        # Add an empty line after each segment
+        srt_text.append("")
+
+    return "\n".join(srt_text)
+
+
+def format_time(seconds) -> str:
+    """Convert time in seconds to SRT time format"""
+    hours, remainder = divmod(seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    milliseconds = int((seconds - int(seconds)) * 1000)
+    return f"{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d},{milliseconds:03d}"
+
+
+def transcribed_text(segments) -> str:
+    """
+    Get the transcribed text from the segments.
+
+    Parameters
+    ----------
+        segments : list
+            List of segments from whisper model.
+
+    Returns
+    -------
+        str
+            Transcribed text from segments.
+    """
+    texts = [s["text"] for s in segments]
+    text = "\n".join(texts)
+    return text
+
+
+def generate_summary(transcript: str, length: int) -> str:
+    """
+    Generate a summary of the transcript using OpenAI's gpt-4 model as YouTube
+    timestamps.
+
+    The summary should be a set of YouTube timestamps that can be used as key
+    points across all segments.
+
+    Parameters
+    ----------
+        transcript : str
+            Transcript of the video.
+        length : int
+
+    Returns
+    -------
+        str
+            Summary of the transcript as YouTube timestamps.
+    """
+    response = client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {
+                "role": "system",
+                "content": """You are a helpful assistant that will summarize
+                           audio transcripts and generate YouTube video
+                           timestamps for important topics in the transcript.
+                           You will adhere to YouTube video timestamp format.
+                           The format expects the timestamps to be formatted as
+                           `HH:MM:SS` for videos longer than an hour and as `MM:SS`
+                           for videos less than an hour. For example, a video
+                           with a length of 00:59:59 (provided as `HH:MM:SS`)
+                           would have timestamps formatted as `MM:SS`, with a min
+                           value of 00:00 and a max value of 59:59. 
+                           
+                           The timestamps should consider segments in the
+                           transcript but not include each segment but only
+                           summarize the general themes. The timestamps should
+                           not exceed the full length of the video. Segments
+                           will be provided in SRT format `HH:MM:SS,MS -->
+                           HH:MM:SS,MS` to assist with YouTube timestamp chapter
+                           generation. You should not generate a summary for
+                           each SRT segment, but rather, generate a set of
+                           summary YouTube timestamps that can be used as key
+                           points across all segments. 
+                           
+                           For example, a video with
+                           a length of 00:20:00 might have 8-10 total timestamps
+                           with the timestamp format `MM:SS`. It might have a
+                           set of input text that follows the format: 
+                           ```
+                           1
+                           00:00:00,000 --> 00:01:00,000
+                           This is the first sentence of the first segment.
+                           
+                           2
+                           00:01:00,000 --> 00:01:15,000
+                           This is the second sentence of the second segment.
+                           
+                           3
+                           00:01:15,000 --> 00:02:20,000
+                           This is the third sentence of the third segment.
+                           ```
+                           Which would result in a set of timestamps like:
+                           ```
+                           0:00 - Introduction
+                           1:15 - Topic overview
+                           etc.
+                           ```
+                """,
+            },
+            {
+                "role": "user",
+                "content": f"The following transcript is for a video of length {length}:\n {transcript}",
+            },
+        ],
+        temperature=0.7,
+    )
+    return response.choices[0].message.content.strip()
+
+
+def main():
+    """
+    Transcribe a video file and generate YouTube timestamps.
+    - Get the video file path from the command line.
+    - Get the duration of the video file.
+    - Transcribe the video file.
+    - Convert the segments to SRT format.
+    - Generate a summary of the transcript as YouTube timestamps.
+    """
+    # Get video file path
+    video_path = cmd()
+    duration = get_video_duration(video_path)
+    print(f"Video duration: {duration}")
+
+    # Transcribe video and convert to SRT
+    transcript = transcribe(video_path)
+    segments = segments_to_srt(transcript["segments"])
+    # Write segments to file
+    segments_file = "segments.srt"
+    with open(segments_file, "w") as f:
+        print(f"Writing segments SRT file to: {segments_file}")
+        f.write(segments)
+    summary = generate_summary(segments, duration)
+
+    # Write summary to file
+    timestamps_file = "timestamps.txt"
+    with open(timestamps_file, "w") as f:
+        print(f"Writing YouTube timestamps file to: {timestamps_file}")
+        f.write(summary)
+
+
+if __name__ == "__main__":
+    main()
